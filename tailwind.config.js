31bfce169edf3e84c3e530a247db0d45f4a1c8f9
ironbeardx <alejandroyeroval@gmail.com>
2023-11-16 04:42:29 -0500

    Agregando colores personalizados a la conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary :'#D6E8FD',
+        secondary : '#9AA0A8',
+        terciary : '#242A43',
+        text : '#111827',
+        accent: '#64030D',
+      },
+    },
   },
   plugins: [],
 }
